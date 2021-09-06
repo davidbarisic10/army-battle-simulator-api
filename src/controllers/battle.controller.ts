5b8e4aace44610536f0c2bd6b9e7229d0ad66974
David Barisic <David.Barisic@aplaceformom.com>
2021-09-06 14:51:10 +0200

    Added some validation to api

--- src/controllers/battle.controller.ts
+++ src/controllers/battle.controller.ts
@@ -7,7 +7,14 @@ export class BattleController {
 
     constructor(private readonly ArmyService: ArmyService, private readonly DiseaseService: DiseaseService, private readonly BattleService: BattleService) { }
     @Get('?')
-    async getQueryParams(@Query() query: any): Promise<Army> {
+    async getQueryParams(@Query() query: any): Promise<any> {
+
+        let validationRegEx = /^[0-9]+$/;
+
+        if(!query.army1.match(validationRegEx || !query.army2.match(validationRegEx))) {
+            return 'INVALID DATA SUPPLIED'!
+        }
+        else{
         let solidersCount = [0,0];
         solidersCount = [parseInt(query.army1), parseInt(query.army2)];
 
@@ -19,5 +26,6 @@ export class BattleController {
         console.log(disease)
 
         return battle;
+        }
     }
 }
